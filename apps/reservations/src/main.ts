54a005c58f4cd781292217e9468e115c4a31ae0d
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-12 21:15:58 +0100

    feat: add Dockerfile and docker compose file

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
-import { Logger } from '@nestjs/common';
+import { Logger as NestLogger, ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const logger = new Logger(bootstrap.name);
+  const logger = new NestLogger(bootstrap.name);
   const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   const PORT = process.env.port ?? 3000;
-  await app.listen(PORT, () => logger.log(`Starting application: ${PORT}`));
+  await app.listen(PORT, () => logger.log(`Starting application: [${PORT}]!`));
 }
 bootstrap();
