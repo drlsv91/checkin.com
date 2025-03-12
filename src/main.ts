d5ada4fb60dd9bface593d6dfbf46d1860ea025c
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-12 15:10:31 +0100

    feat: add config and common library

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger(bootstrap.name);
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT ?? 3000;
+  await app.listen(PORT, () => logger.log(`Starting Application on ${PORT}`));
 }
 bootstrap();
