ff3e97db2409476b85c9f60de24f765a6e507cce
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-13 15:56:33 +0100

    feat: add jwt auth strategy

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -3,10 +3,12 @@ import { AuthModule } from './auth.module';
 import { Logger as NestLogger, ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const logger = new NestLogger(bootstrap.name);
   const app = await NestFactory.create(AuthModule);
+  app.use(cookieParser());
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
