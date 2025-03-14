49278f79604167a89921aa7da39390d3a9bb8f9b
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-03-14 21:14:58 +0100

    feat: enable cookie parsing in the reservations application

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -8,13 +8,13 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const logger = new NestLogger(bootstrap.name);
   const app = await NestFactory.create(ReservationsModule);
-  app.use(cookieParser());
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
+  app.use(cookieParser());
   const PORT = configService.get('PORT');
   await app.listen(PORT, '0.0.0.0', () =>
-    logger.log(`Starting application: [${PORT}]!`),
+    logger.log(`Starting application: [${PORT}]`),
   );
 }
 bootstrap();
